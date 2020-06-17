363b1dd9f8f98a2975931268cd7cb5d20f955a59
troubleshooter495 <daymanvaa@mail.ru>
2020-06-17 13:47:45 +0300

    final fixes

---
+++ version1/main.js
@@ -0,0 +1,153 @@
+"use strict";
+
+function main() {
+    var canvas = document.getElementById("mainCanvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    var program = webglUtils.createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    var colorLocation = gl.getUniformLocation(program, "u_color");
+    var translationLocation = gl.getUniformLocation(program, "u_translation");
+    var scaleLocation = gl.getUniformLocation(program, "u_scale");
+    var positionBuffer = gl.createBuffer();
+    var rotationLocation = gl.getUniformLocation(program, "u_rotation");
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    setGeometry(gl);
+
+    var translation = [0, 0];
+    var scale = [1, 1];
+    var rotation = [0, 1];
+    var width = 100;
+    var height = 30;
+    var color = [Math.random(), Math.random(), Math.random(), 1];
+
+    drawScene();
+
+    webglLessonsUI.setupSlider("#x", { slide: updatePosition(0), max: gl.canvas.width });
+    webglLessonsUI.setupSlider("#y", { slide: updatePosition(1), max: gl.canvas.height });
+    webglLessonsUI.setupSlider("#r", { slide: updateAngle, max: 360 });
+    webglLessonsUI.setupSlider("#s", { slide: updateSize, max: 1000 });
+
+    canvas.addEventListener('mousedown', e => {
+        var scalingCoefficient = 1.1;
+        scale[0] = scalingCoefficient;
+        scale[1] = scalingCoefficient;
+        drawScene();
+    });
+    
+    function updatePosition(index) {
+        return function(event, ui) {
+            translation[index] = ui.value;
+            drawScene();
+        }
+    }
+
+    function updateAngle(event, ui) {
+        var angleInDegrees = ui.value;
+        var angleInRadians = angleInDegrees * Math.PI / 180;
+        rotation[0] = Math.sin(angleInRadians);
+        rotation[1] = Math.cos(angleInRadians);
+        drawScene();
+    }
+
+    function updateSize(event, ui) {
+        var scalingCoefficient = ui.value / 100;
+        scale[0] = scalingCoefficient;
+        scale[1] = scalingCoefficient;
+        drawScene();
+    }
+
+    function drawScene() {
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.useProgram(program);
+        gl.enableVertexAttribArray(positionLocation);
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        var size = 2;
+        var type = gl.FLOAT;
+        var normalize = false;
+        var stride = 0;
+        var offset = 0;
+        gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset);
+        gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+        gl.uniform2fv(rotationLocation, rotation);
+        gl.uniform2fv(scaleLocation, scale);
+        gl.uniform4fv(colorLocation, color);
+        gl.uniform2fv(translationLocation, translation);
+
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 18;
+        gl.drawArrays(primitiveType, offset, count);
+    }
+
+    function setGeometry(gl) {
+        gl.bufferData(gl.ARRAY_BUFFER,
+            new Float32Array([
+                // вертикальный столб
+                0, 0,
+                30, 0,
+                0, 150,
+                0, 150,
+                30, 0,
+                30, 150,
+
+                // верхняя перекладина
+                30, 0,
+                100, 0,
+                30, 30,
+                30, 30,
+                100, 0,
+                100, 30,
+
+                // перекладина посередине
+                30, 60,
+                67, 60,
+                30, 90,
+                30, 90,
+                67, 60,
+                67, 90,
+            ]),
+            gl.STATIC_DRAW);
+    }
+}
+
+
+
+
+function setRectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2
+    ]), gl.STATIC_DRAW);
+}
+
+function fiftySquares(gl, colorUniformLocation) { //example1 draws 50 random rectangles
+    for (var ii = 0; ii < 50; ++ii) {
+        setRectangle(gl, randomInt(500), randomInt(500), randomInt(500), randomInt(500));
+        gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+}
+
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
+
+main();
